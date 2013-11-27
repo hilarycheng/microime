3069c78aa0df7d8a9aa6761767ee649d26c94801
Hilary Cheng <hilarycheng@gmail.com>
2013-11-27 23:43:25 +0800

    - Add Swipe on Space Key for Input Method Selection ( Only Testing Code   )

--- src/com/diycircuits/microime/KbView.java
+++ src/com/diycircuits/microime/KbView.java
@@ -9,6 +9,7 @@ import android.graphics.Rect;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.FontMetricsInt;
+import android.widget.PopupWindow;
 import android.view.Display;
 import android.view.WindowManager;
 import android.view.MotionEvent;
@@ -17,6 +18,8 @@ import android.util.SparseArray;
 import android.graphics.Typeface;
 import java.util.ArrayList;
 import android.graphics.Rect;
+import android.view.LayoutInflater;
+import android.view.Gravity;
 
 public class KbView extends View {
 
@@ -29,9 +32,13 @@ public class KbView extends View {
     private KeyListener mListener = null;
     private ArrayList<Key> mPressedKey = new ArrayList<Key>();
     private Rect mDirtyBound = new Rect();
+    private PopupWindow mPopup = new PopupWindow();
+    private final Context mContext;
 
     public KbView(Context context, AttributeSet attrs) {
     	super(context, attrs);
+
+	mContext = context;
     	mNormal = getResources().getDrawable(R.drawable.btn_keyboard_key_ics);
 
         mPaint = new Paint();
@@ -55,6 +62,7 @@ public class KbView extends View {
     public boolean onTouchEvent(MotionEvent event) {
 	super.onTouchEvent(event);
 
+	Log.i("MicroIME", "TouchEvent " + event);
 	final Keyboard keyboard = KeyboardState.getInstance().getCurrentKeyboard();
 	for (int row = 0; row < keyboard.getRow(); row++) {
 	    boolean inter = keyboard.getRow(row).mBounds.contains((int) event.getX(), (int) event.getY());
@@ -68,7 +76,22 @@ public class KbView extends View {
 		    mPressedKey.add(key);
 		    key.setPressed();
 		    invalidate(key.mBounds);
+		    if (key.mType == KeyType.SPACE) {
+			Log.i("MicroIME", "Key Type Popup");
+			LayoutInflater inflate = (LayoutInflater)
+			    mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			mPopup = new PopupWindow();
+			mPopup.setContentView(inflate.inflate(R.layout.inputmethod, null));
+			mPopup.setWidth(100);
+			mPopup.setHeight(100);
+			mPopup.setSoftInputMode(PopupWindow.INPUT_METHOD_NOT_NEEDED);
+			mPopup.showAtLocation((View) this, Gravity.NO_GRAVITY, (int) (key.mBounds.right - key.mBounds.left - 100) / 2 + key.mBounds.left, (int) key.mBounds.top - 100);
+		    }
 		} else if (event.getAction() == MotionEvent.ACTION_UP && mListener != null) {
+		    if (mPopup != null) {
+			mPopup.dismiss();
+			mPopup = null;
+		    }
 		    key.setRelease();
 		    mDirtyBound.set(key.mBounds);
 		    for (int i = 0; i < mPressedKey.size(); i++) {
@@ -83,6 +106,10 @@ public class KbView extends View {
 	}
 
 	if (event.getAction() == MotionEvent.ACTION_UP) {
+	    if (mPopup != null) {
+		mPopup.dismiss();
+		mPopup = null;
+	    }
 	    for (int i = 0; i < mPressedKey.size(); i++) {
 		if (i == 0) 
 		    mDirtyBound.set(mPressedKey.get(i).mBounds);
